045db81e4746542624890a9c694b5dacaba2432c
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2017-11-13 18:26:04 -0500

    Added data object creation methods.

--- Assets/DxR/Scripts/Parser.cs
+++ Assets/DxR/Scripts/Parser.cs
@@ -9,10 +9,36 @@ namespace DxR
 {
     public class Parser
     {
+        private bool verbose = false;           // Set to true to display debugging info.
 
-        public bool Parse(string specsFilename, ref JSONNode sceneSpecs)
+        /// <summary>
+        ///  Read specifications in JSON file specified by specsFilename as 
+        ///  well as data file (if needed) and expand to a JSONNode scene specs with the 
+        ///  data represented as a JSON object.
+        /// </summary>
+        public void Parse(string specsFilename, out JSONNode sceneSpecs)
         {
-            return true;
+           sceneSpecs = JSON.Parse(GetStringFromFile(specsFilename));
+
+           ExpandDataSpecs(ref sceneSpecs);
+        }
+
+        private void ExpandDataSpecs(ref JSONNode sceneSpecs)
+        {
+            if (sceneSpecs["data"]["url"] != null)
+            {
+                string dataFilename = sceneSpecs["data"]["url"];
+                JSONNode valuesJSONNode = JSON.Parse(GetStringFromFile(dataFilename));
+                sceneSpecs["data"].AsObject.Add("values", valuesJSONNode);
+            }
+
+            // TODO: Do some checks.
+        }
+
+        private string GetStringFromFile(string filename)
+        {
+            string filePath = Path.Combine(Application.streamingAssetsPath, filename);
+            return File.ReadAllText(filePath);
         }
     }
 }
--- Assets/DxR/Scripts/SceneObject.cs
+++ Assets/DxR/Scripts/SceneObject.cs
@@ -16,6 +16,7 @@ namespace DxR
      /// </summary>
     public class SceneObject : MonoBehaviour
     {
+        private bool verbose = true;
         public string specsFilename = "DxRData/example.json";
         public JSONNode sceneSpecs;
 
@@ -35,7 +36,7 @@ namespace DxR
         {
             sceneRoot = gameObject;
 
-            Parse(specsFilename, ref sceneSpecs);
+            Parse(specsFilename, out sceneSpecs);
 
             Infer(ref sceneSpecs);
             
@@ -45,24 +46,83 @@ namespace DxR
         // Parse (JSON spec file (data file info in specs) -> expanded raw JSON specs): 
         // Read in the specs and data files to create expanded raw JSON specs.
         // Filenames should be relative to Assets/StreamingAssets/ directory.
-        public void Parse(string specsFilename, ref JSONNode sceneSpecs)
+        private void Parse(string specsFilename, out JSONNode sceneSpecs)
         {
             Parser parser = new Parser();
-            parser.Parse(specsFilename, ref sceneSpecs);
+            parser.Parse(specsFilename, out sceneSpecs);
         }
 
         // Infer (raw JSON specs -> full JSON specs): 
         // automatically fill in missing specs by inferrence (informed by marks and data type).
-        public void Infer(ref JSONNode sceneSpecs)
+        private void Infer(ref JSONNode sceneSpecs)
         {
 
         }
         
         // Construct (full JSON specs -> working SceneObject): 
-        public void Construct(JSONNode sceneSpec, ref GameObject sceneRoot)
+        private void Construct(JSONNode sceneSpecs, ref GameObject sceneRoot)
         {
+            CreateDataObjectFromValues(sceneSpecs["data"]["values"], out data);
 
         }
+
+        private void CreateDataObjectFromValues(JSONNode valuesSpecs, out Data data)
+        {
+            data = new Data();
+
+            CreateDataFields(valuesSpecs, ref data);
+
+            data.values = new List<Dictionary<string, string>>();
+
+            int numDataFields = data.fieldNames.Count;
+            if(verbose)
+            {
+                Debug.Log("Counted " + numDataFields.ToString() + " fields in data.");
+            }
+
+            // Loop through the values in the specification
+            // and insert one Dictionary entry in the values list for each.
+            foreach (JSONNode value in valuesSpecs.Children)
+            {
+                Dictionary<string, string> d = new Dictionary<string, string>();
+
+                bool valueHasNullField = false;
+                for (int fieldIndex = 0; fieldIndex < numDataFields; fieldIndex++)
+                {
+                    string curFieldName = data.fieldNames[fieldIndex];
+
+                    // TODO: Handle null / missing values properly.
+                    if (value[curFieldName].IsNull)
+                    {
+                        valueHasNullField = true;
+                        Debug.Log("value null found: ");
+                        break;
+                    }
+
+                    d.Add(curFieldName, value[curFieldName]);
+                }
+
+                if (!valueHasNullField)
+                {
+                    data.values.Add(d);
+                }
+            }
+        }
+
+        private void CreateDataFields(JSONNode valuesSpecs, ref Data data)
+        {
+            data.fieldNames = new List<string>();
+            foreach (KeyValuePair<string, JSONNode> kvp in valuesSpecs[0].AsObject)
+            {
+                data.fieldNames.Add(kvp.Key);
+
+                if(verbose)
+                {
+                    Debug.Log("Reading data field: " + kvp.Key);
+                }
+            }
+        }
+
     }
 }
 
