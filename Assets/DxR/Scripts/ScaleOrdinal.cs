123066481ac54f350a35b0c1df2d2557249fb639
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2017-11-16 16:58:11 -0500

    Working prototype of ordinal scale with ordinal rage scheme.
    -currently only uses two colors in the range

--- Assets/DxR/Scripts/ScaleOrdinal.cs
+++ Assets/DxR/Scripts/ScaleOrdinal.cs
@@ -21,6 +21,10 @@ namespace DxR
                         SetupScaleCategory(scaleSpecs);
                         break;
 
+                    case "ordinal":
+                        SetupScaleOrdinal(scaleSpecs);
+                        break;
+
                     default:
                         throw new System.Exception("Invalid range " + rangeType
                             +" for ordinal scale type.");
@@ -39,6 +43,18 @@ namespace DxR
             {
                 LoadColorScheme(scaleSpecs["scheme"].Value.ToString(), ref base.range);
             } else
+            {
+                throw new System.Exception("Missing scheme spec in ordinal scale spec.");
+            }
+        }
+
+        private void SetupScaleOrdinal(JSONNode scaleSpecs)
+        {
+            if (scaleSpecs["scheme"] != null)
+            {
+                LoadColorScheme(scaleSpecs["scheme"].Value.ToString(), ref base.range);
+            }
+            else
             {
                 throw new System.Exception("Missing scheme spec in ordinal scale spec.");
             }
@@ -66,6 +82,9 @@ namespace DxR
                 case "category":
                     return ApplyScaleCategory(domainValue);
 
+                case "ordinal":
+                    return ApplyScaleOrdinal(domainValue);
+
                 default:
                     throw new Exception("Invalid range type for ordinal scale.");
             }           
@@ -89,6 +108,47 @@ namespace DxR
             Debug.Log("Scaling " + domainValue + " to " + rangeValue);
 
             return rangeValue;
+        }
+
+        // This currently only interpolates the output color using the first two
+        // colors in the range.
+        // TODO: Handle more complex schemes, e.g., blues-3, blues-4, ... 
+        // see: https://vega.github.io/vega-lite/docs/scale.html#scheme
+        private string ApplyScaleOrdinal(string domainValue)
+        {
+            string rangeValue = base.range[0];
+
+            int domainValueIndex = base.domain.IndexOf(domainValue);
+
+            if (domainValueIndex == -1)
+            {
+                throw new System.Exception("Invalid domain value " + domainValue);
+            }
+            else
+            {
+                if(domainValueIndex == 0)
+                {
+                    return base.range[0];
+                } else if(domainValueIndex == base.domain.Count - 1)
+                {
+                    return base.range[1];
+                } else
+                {
+                    float pct = (float)(domainValueIndex) / (float)(base.domain.Count - 1);
+
+                    Color startColor;
+                    Color endColor;
+
+                    ColorUtility.TryParseHtmlString(base.range[0], out startColor);
+                    ColorUtility.TryParseHtmlString(base.range[1], out endColor);
+
+                    Color lerpedColor = Color.Lerp(startColor, endColor, pct);
+
+                    string col = "#" + ColorUtility.ToHtmlStringRGB(lerpedColor);
+
+                    return col;
+                }
+            }
         }
     }
 }
