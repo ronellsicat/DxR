898456b05523902ff2e61f4e1154dce49f01672b
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2017-11-20 18:19:37 -0500

    Fixed opacity channel setting.

--- Assets/DxR/Scripts/Mark.cs
+++ Assets/DxR/Scripts/Mark.cs
@@ -171,9 +171,22 @@ namespace DxR
 
         private void SetOpacity(string value)
         {
+            SetRenderModeToTransparent(transform.GetComponent<Renderer>().material);
             Color color = transform.GetComponent<Renderer>().material.color;
             color.a = float.Parse(value);
             transform.GetComponent<Renderer>().material.color = color;
+        }
+
+        private void SetRenderModeToTransparent(Material m)
+        {
+            m.SetFloat("_Mode", 2);
+            m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            m.SetInt("_ZWrite", 0);
+            m.DisableKeyword("_ALPHATEST_ON");
+            m.EnableKeyword("_ALPHABLEND_ON");
+            m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            m.renderQueue = 3000;
         }
     }
 }
