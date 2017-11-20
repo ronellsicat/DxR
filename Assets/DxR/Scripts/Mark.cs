e7b11575d5efd29d3ea4e8f262ccc9d9a7e05d72
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2017-11-20 15:59:36 -0500

    Removed BarMark script - moved all bar mark script functionalities to Mark base class mark.

--- Assets/DxR/Scripts/Mark.cs
+++ Assets/DxR/Scripts/Mark.cs
@@ -22,43 +22,149 @@ namespace DxR
             switch(channel)
             {
                 case "x":
-                    SetX(value);
+                    TranslateBy(value, 0);
                     break;
                 case "y":
-                    SetY(value);
+                    TranslateBy(value, 1);
                     break;
-                case "z":
-                    SetZ(value);
+                   case "z":
+                    TranslateBy(value, 2);
                     break;
+                 case "width":
+                    SetSize(value, 0);
+                    break;
+                case "height":
+                    SetSize(value, 1);
+                    break;
+                case "depth":
+                    SetSize(value, 2);
+                    break;
+                case "xoffset":
+                    SetOffset(value, 0);
+                    break;
+                case "yoffset":
+                    SetOffset(value, 1);
+                    break;
+                case "zoffset":
+                    SetOffset(value, 2);
+                    break;
+                case "xoffsetpct":
+                    SetOffsetPct(value, 0);
+                    break;
+                case "yoffsetpct":
+                    SetOffsetPct(value, 1);
+                    break;
+                case "zoffsetpct":
+                    SetOffsetPct(value, 2);
+                    break;
+                case "color":
+                    SetColor(value);
+                    break;
+                case "size":
+                    SetMaxSize(value);
+                    break;
+                case "xrotation":
+                    SetRotation(value, 0);
+                    break;
+                case "yrotation":
+                    SetRotation(value, 1);
+                    break;
+                case "zrotation":
+                    SetRotation(value, 2);
+                    break;
+                case "x2":
+                    throw new System.Exception("x2 is not a valid channel - use x, and width instead.");
+                case "y2":
+                    throw new System.Exception("y2 is not a valid channel - use y, and height instead.");
+                case "z2":
+                    throw new System.Exception("z2 is not a valid channel - use z, and depth instead.");
                 default:
                     throw new System.Exception("Cannot find channel: " + channel);
             }
         }
 
-        private void SetX(string value)
+        private void TranslateBy(string value, int dim)
         {
-            //Debug.Log("setting x value");
-            float targetX = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
-            float x = gameObject.transform.localPosition.x;
-            transform.Translate(targetX - x, 0, 0);
+            // TODO: Do this more robustly.
+            float pos = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
+
+            Vector3 localPos = gameObject.transform.localPosition;
+            Vector3 translateBy = Vector3.zero;
+            translateBy[dim] = pos - localPos[dim];
+            gameObject.transform.Translate(translateBy);
         }
 
-        private void SetY(string value)
+        private void SetSize(string value, int dim)
         {
-            //Debug.Log("setting y value");
-            float targetY = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
-            float y = gameObject.transform.localPosition.y;
-            gameObject.transform.Translate(0, targetY - y, 0);
+            float size = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
+
+            Vector3 initPos = transform.localPosition;
+
+            Vector3 curScale = transform.localScale;
+            GetComponent<MeshFilter>().mesh.RecalculateBounds();
+            Vector3 origMeshSize = GetComponent<MeshFilter>().mesh.bounds.size;
+            curScale[dim] = size / (origMeshSize[dim]);
+            transform.localScale = curScale;
+
+            transform.localPosition = initPos;  // This handles models that get translated with scaling.
         }
 
-        private void SetZ(string value)
+        private void SetOffset(string value, int dim)
         {
-            //Debug.Log("setting z value");
-            float targetZ = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
-            float z = gameObject.transform.localPosition.z;
-            transform.Translate(0, 0, targetZ - z);
+            float offset = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
+            Vector3 translateBy = transform.localPosition;
+            translateBy[dim] = offset - translateBy[dim];
+            transform.localPosition = translateBy;
         }
 
+        private void SetOffsetPct(string value, int dim)
+        {
+            GetComponent<MeshFilter>().mesh.RecalculateBounds();
+            float offset = float.Parse(value) * GetComponent<MeshFilter>().mesh.bounds.size[dim] *
+                gameObject.transform.localScale[dim];
+            Vector3 translateBy = transform.localPosition;
+            translateBy[dim] = offset - translateBy[dim];
+            transform.localPosition = translateBy;
+        }
+
+        private void SetRotation(string value, int dim)
+        {
+            Vector3 rot = transform.localEulerAngles;
+            rot[dim] = float.Parse(value);
+            transform.localEulerAngles = rot;
+        }
+
+        private void SetMaxSize(string value)
+        {
+            float size = float.Parse(value) * DxR.SceneObject.SIZE_UNIT_SCALE_FACTOR;
+
+            Vector3 renderSize = gameObject.transform.GetComponent<Renderer>().bounds.size;
+            Vector3 localScale = gameObject.transform.localScale;
+
+            int maxIndex = 0;
+            float maxSize = renderSize[maxIndex];
+            for(int i = 1; i < 3; i++)
+            {
+                if(maxSize < renderSize[i])
+                {
+                    maxSize = renderSize[i];
+                    maxIndex = i;
+                }
+            }
+
+            float origMaxSize = renderSize[maxIndex] / localScale[maxIndex];
+            float newLocalScale = (size / origMaxSize);
+
+            gameObject.transform.localScale = new Vector3(newLocalScale,
+                newLocalScale, newLocalScale);
+        }
+
+        private void SetColor(string value)
+        {
+            Color color;
+            bool colorParsed = ColorUtility.TryParseHtmlString(value, out color);
+            transform.GetComponent<Renderer>().material.color = color;
+        }
     }
 
 }
