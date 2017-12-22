aff6f94f477a3fe6f1853c100274f425a6a56bf8
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2017-12-21 22:52:41 -0500

    Added barebones audio mark example.

---
+++ Assets/DxR/Resources/Marks/audio/pitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pitch : MonoBehaviour {
+
+    public float minY;
+    public float maxY;
+    void Start () {
+		
+	}
+	
+	void Update () {
+        gameObject.GetComponent<AudioSource>().pitch = (transform.position.y - minY) / (-3 - minY) * (3 - maxY) + maxY;
+	}
+
+    
+}
