4c8565b61490f0129d4e5f06aa4de7427e1d69f4
DESKTOP-T6DIR49\rsicat <ronell.sicat@gmail.com>
2018-02-01 14:09:29 -0500

    Added leap motion switch

--- Assets/DxR/Scripts/Vis.cs
+++ Assets/DxR/Scripts/Vis.cs
@@ -20,6 +20,7 @@ namespace DxR
         public bool enableSpecsExpansion = false;                       // Switch for automatically replacing the vis specs text file on disk with inferrence result.
         public bool enableTooltip = true;                               // Switch for tooltip that shows datum attributes on-hover of mark instance.
         public bool verbose = true;                                     // Switch for verbose log.
+        public bool enableLeapMotion = false;                           // Switch for enabling leap motion based interaction.
 
         public static string UNDEFINED = "undefined";                   // Value used for undefined objects in the JSON vis specs.
         public static float SIZE_UNIT_SCALE_FACTOR = 1.0f / 1000.0f;    // Conversion factor to convert each Unity unit to 1 meter.
