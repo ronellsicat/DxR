b6356ebb65469144a90d12f74118e17b14b2a6c4
Henri J. Norden <55378880+Henri-J-Norden@users.noreply.github.com>
2022-03-10 10:48:58 +0100

    Update to new input system (classics example scene only)

--- Assets/External/Gaze UI/Scripts/CameraController.cs
+++ Assets/External/Gaze UI/Scripts/CameraController.cs
@@ -1,177 +1,129 @@
 ﻿//
 // Source : https://wiki.unity3d.com/index.php/SmoothMouseLook
 //
- 
+
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
- 
+using UnityEngine.InputSystem;
+using UnityEngine.Assertions;
+
 [AddComponentMenu("Camera-Control/Camera Controller")]
 public class CameraController : MonoBehaviour {
 
-    /*
-    Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
-    Converted to C# 27-02-13 - no credit wanted.
-    Simple flycam I made, since I couldn't find any others made public.  
-    Made simple to use (drag and drop, done) for regular keyboard layout  
-    wasd : basic movement
-    shift : Makes camera accelerate
-    space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+	/*
+	Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.  
+	Converted to C# 27-02-13 - no credit wanted.
+	Simple flycam I made, since I couldn't find any others made public.  
+	Made simple to use (drag and drop, done) for regular keyboard layout  
+	wasd : basic movement
+	shift : Makes camera accelerate
+	space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
 
+	[Header("Keyboard")]
+	public float mainSpeed = 1.0f; //regular speed
+	public float shiftAdd = 2.0f; //multiplied by how long shift is held.  Basically running
+	public float maxShift = 10.0f; //Maximum speed when holdin gshift
+	private float totalRun = 1.0f;
 
-    float mainSpeed = 1.0f; //regular speed
-    float shiftAdd = 2.0f; //multiplied by how long shift is held.  Basically running
-    float maxShift = 10.0f; //Maximum speed when holdin gshift
-    float camSens = 1.0f; //How sensitive it with mouse
-    private float totalRun = 1.0f;
+	public InputActionReference forward, backward, left, right, run, vertical;
 
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-	public RotationAxes axes = RotationAxes.MouseXAndY;
+	[Header("Mouse")]
+	public InputActionReference X_axis;
+	public InputActionReference Y_axis;
 	public float sensitivityX = 15F;
 	public float sensitivityY = 15F;
- 
+
 	public float minimumX = -360F;
 	public float maximumX = 360F;
- 
+
 	public float minimumY = -60F;
 	public float maximumY = 60F;
- 
+
 	float rotationX = 0F;
 	float rotationY = 0F;
- 
+
 	private List<float> rotArrayX = new List<float>();
-	float rotAverageX = 0F;	
- 
+	float rotAverageX = 0F;
+
 	private List<float> rotArrayY = new List<float>();
 	float rotAverageY = 0F;
- 
+
 	public float frameCounter = 20;
- 
+
 	Quaternion originalRotation;
- 
-	void Update ()
-	{
-		if (axes == RotationAxes.MouseXAndY)
-		{			
-			rotAverageY = 0f;
-			rotAverageX = 0f;
- 
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
- 
-			rotArrayY.Add(rotationY);
-			rotArrayX.Add(rotationX);
- 
-			if (rotArrayY.Count >= frameCounter) {
-				rotArrayY.RemoveAt(0);
-			}
-			if (rotArrayX.Count >= frameCounter) {
-				rotArrayX.RemoveAt(0);
-			}
- 
-			for(int j = 0; j < rotArrayY.Count; j++) {
-				rotAverageY += rotArrayY[j];
-			}
-			for(int i = 0; i < rotArrayX.Count; i++) {
-				rotAverageX += rotArrayX[i];
-			}
- 
-			rotAverageY /= rotArrayY.Count;
-			rotAverageX /= rotArrayX.Count;
- 
-			rotAverageY = ClampAngle (rotAverageY, minimumY, maximumY);
-			rotAverageX = ClampAngle (rotAverageX, minimumX, maximumX);
- 
-			Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
-			Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
- 
-			transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+
+	void Update() {
+		rotAverageY = 0f;
+		rotAverageX = 0f;
+		
+		rotationY += Y_axis?.action.ReadValue<float>() ?? 0 * sensitivityY;
+		rotationX += X_axis?.action.ReadValue<float>() ?? 0 * sensitivityX;
+
+		rotArrayY.Add(rotationY);
+		rotArrayX.Add(rotationX);
+
+		if (rotArrayY.Count >= frameCounter) {
+			rotArrayY.RemoveAt(0);
 		}
-		else if (axes == RotationAxes.MouseX)
-		{			
-			rotAverageX = 0f;
- 
-			rotationX += Input.GetAxis("Mouse X") * sensitivityX;
- 
-			rotArrayX.Add(rotationX);
- 
-			if (rotArrayX.Count >= frameCounter) {
-				rotArrayX.RemoveAt(0);
-			}
-			for(int i = 0; i < rotArrayX.Count; i++) {
-				rotAverageX += rotArrayX[i];
-			}
-			rotAverageX /= rotArrayX.Count;
- 
-			rotAverageX = ClampAngle (rotAverageX, minimumX, maximumX);
- 
-			Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
-			transform.localRotation = originalRotation * xQuaternion;			
-		}
-		else
-		{			
-			rotAverageY = 0f;
- 
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
- 
-			rotArrayY.Add(rotationY);
- 
-			if (rotArrayY.Count >= frameCounter) {
-				rotArrayY.RemoveAt(0);
-			}
-			for(int j = 0; j < rotArrayY.Count; j++) {
-				rotAverageY += rotArrayY[j];
-			}
-			rotAverageY /= rotArrayY.Count;
- 
-			rotAverageY = ClampAngle (rotAverageY, minimumY, maximumY);
- 
-			Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
-			transform.localRotation = originalRotation * yQuaternion;
+		if (rotArrayX.Count >= frameCounter) {
+			rotArrayX.RemoveAt(0);
 		}
 
-        //Keyboard commands
-        float f = 0.0f;
-        Vector3 p = GetBaseInput();
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            totalRun += Time.deltaTime;
-            p = p * totalRun * shiftAdd;
-            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-        }
-        else
-        {
-            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-            p = p * mainSpeed;
-        }
+		for (int j = 0; j < rotArrayY.Count; j++) {
+			rotAverageY += rotArrayY[j];
+		}
+		for (int i = 0; i < rotArrayX.Count; i++) {
+			rotAverageX += rotArrayX[i];
+		}
 
-        p = p * Time.deltaTime;
-        Vector3 newPosition = transform.position;
-        if (Input.GetKey(KeyCode.Space))
-        { //If player wants to move on X and Z axis only
-            transform.Translate(p);
-            newPosition.x = transform.position.x;
-            newPosition.z = transform.position.z;
-            transform.position = newPosition;
-        }
-        else
-        {
-            transform.Translate(p);
-        }
-    }
- 
-	void Start ()
-	{		
-                Rigidbody rb = GetComponent<Rigidbody>();	
+		rotAverageY /= rotArrayY.Count;
+		rotAverageX /= rotArrayX.Count;
+
+		rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
+		rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+
+		Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
+		Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
+
+		transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+
+
+		//Keyboard commands
+		float f = 0.0f;
+		Vector3 p = GetBaseInput();
+		if (run.action.IsPressed()) {
+			totalRun += Time.deltaTime;
+			p = p * totalRun * shiftAdd;
+			p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+			p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+			p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+		} else {
+			totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+			p = p * mainSpeed;
+		}
+
+		p = p * Time.deltaTime;
+		Vector3 newPosition = transform.position;
+		if (vertical.action.IsPressed()) { //If player wants to move on X and Z axis only
+			transform.Translate(p);
+			newPosition.x = transform.position.x;
+			newPosition.z = transform.position.z;
+			transform.position = newPosition;
+		} else {
+			transform.Translate(p);
+		}
+	}
+
+	void Start() {
+		Rigidbody rb = GetComponent<Rigidbody>();
+
 		if (rb)
 			rb.freezeRotation = true;
 		originalRotation = transform.localRotation;
 	}
- 
-	public static float ClampAngle (float angle, float min, float max)
-	{
+
+	public static float ClampAngle(float angle, float min, float max) {
 		angle = angle % 360;
 		if ((angle >= -360F) && (angle <= 360F)) {
 			if (angle < -360F) {
@@ -179,30 +131,25 @@ public class CameraController : MonoBehaviour {
 			}
 			if (angle > 360F) {
 				angle -= 360F;
-			}			
+			}
 		}
-		return Mathf.Clamp (angle, min, max);
+		return Mathf.Clamp(angle, min, max);
 	}
 
-    private Vector3 GetBaseInput()
-    { //returns the basic values, if it's 0 than it's not active.
-        Vector3 p_Velocity = new Vector3();
-        if (Input.GetKey(KeyCode.W))
-        {
-            p_Velocity += new Vector3(0, 0, 1);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            p_Velocity += new Vector3(0, 0, -1);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            p_Velocity += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            p_Velocity += new Vector3(1, 0, 0);
-        }
-        return p_Velocity;
-    }
+	private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
+		Vector3 p_Velocity = new Vector3();
+		if (forward.action.IsPressed()) {
+			p_Velocity += new Vector3(0, 0, 1);
+		}
+		if (backward.action.IsPressed()) {
+			p_Velocity += new Vector3(0, 0, -1);
+		}
+		if (left.action.IsPressed()) {
+			p_Velocity += new Vector3(-1, 0, 0);
+		}
+		if (right.action.IsPressed()) {
+			p_Velocity += new Vector3(1, 0, 0);
+		}
+		return p_Velocity;
+	}
 }
